332e5b116828ba04f893ca69dcdc1c4d63a3ef33
Luiz <l.eduardo8316@gmail.com>
2024-10-07 21:31:33 -0300

    feat: parse env files

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "drizzle-kit";
+import { env } from "./src/env";
 
 export default defineConfig({
   schema: "./src/db/schema/index.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DB_URL as string,
+    url: env.DATABASE_URL,
   },
   verbose: true,
   strict: true,
--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -1,12 +1,17 @@
 import postgres from "postgres";
 
+import chalk from "chalk";
+
 import { drizzle } from "drizzle-orm/postgres-js";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
+import { env } from "../env";
 
-const connection = postgres(process.env.DB_URL as string, { max: 1 });
+const connection = postgres(env.DATABASE_URL, { max: 1 });
 const db = drizzle(connection);
 
 await migrate(db, { migrationsFolder: "drizzle" });
+
+console.log(chalk.greenBright("Migrations applied succesfully!"));
 
 await connection.end();
 
