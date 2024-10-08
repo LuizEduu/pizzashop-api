85b3b52dc4a01e7fbb5719faa8c03e9b17c2bcac
Luiz <l.eduardo8316@gmail.com>
2024-10-07 22:16:25 -0300

    feat: create seed to tests data

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,16 +1,11 @@
 import { defineConfig } from "drizzle-kit";
-import { env } from "./src/env";
+
+process.env.DATABASE_URL;
 
 export default defineConfig({
   schema: "./src/db/schema/index.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: env.DATABASE_URL,
-  },
-  verbose: true,
-  strict: true,
-  migrations: {
-    table: "migrations",
-    schema: "public",
+    url: process.env.DATABASE_URL as string,
   },
 });
